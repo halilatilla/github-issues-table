8cad84e42f00ea069edd7ebc7c5bc59a30be2a6c
halilatilla <atillahalil0@gmail.com>
2024-04-09 16:51:49 +0300

    Initial 🚀

---
+++ src/lib/utils.ts
@@ -0,0 +1,44 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getReadableTextColor(backgroundColor: string) {
+  const r = parseInt(backgroundColor.slice(0, 2), 16);
+  const g = parseInt(backgroundColor.slice(2, 4), 16);
+  const b = parseInt(backgroundColor.slice(4, 6), 16);
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? "black" : "white";
+}
+
+export function timeSince(date: Date): string {
+  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+
+  let interval = seconds / 31536000; // 365 * 24 * 60 * 60
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000; // 30 * 24 * 60 * 60
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 86400; // 24 * 60 * 60
+  if (interval >= 2) {
+    return Math.floor(interval) + " days ago";
+  }
+  if (interval >= 1) {
+    return "yesterday";
+  }
+  interval = seconds / 3600; // 60 * 60
+  if (interval >= 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60;
+  if (interval >= 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return "just now"; // For anything less than a minute
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
 import type { AppProps } from "next/app";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >
+      <Component {...pageProps} />;
+    </main>
+  );
 }
