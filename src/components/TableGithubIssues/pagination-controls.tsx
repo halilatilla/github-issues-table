b4e505bfa11c4a5ce008b18ddb2117c77c2e1cd9
halilatilla <atillahalil0@gmail.com>
2024-04-11 15:55:14 +0300

    Add PaginationControls component

---
+++ src/components/TableGithubIssues/pagination-controls.tsx
@@ -0,0 +1,33 @@
+import { Table } from '@tanstack/react-table';
+
+import { Button } from '@/components/ui/button';
+import { TIssue } from '@/types';
+
+interface PaginationControlsProps {
+  table: Table<TIssue>;
+}
+
+const PaginationControls = ({ table }: PaginationControlsProps) => (
+  <div className="flex justify-end space-x-2 py-4">
+    <div className="space-x-2">
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+      >
+        Previous
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+      >
+        Next
+      </Button>
+    </div>
+  </div>
+);
+
+export default PaginationControls;
